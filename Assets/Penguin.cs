7a13b9ce6d23ccf53ab39c9973b526c004a9f84c
U-Gabriel <gabrielarie2000@gmail.com>
2024-12-18 22:20:17 +0100

    Update - Mov Player

--- Assets/Penguin.cs
+++ Assets/Penguin.cs
@@ -4,73 +4,61 @@ using UnityEngine;
 
 public class Penguin : MonoBehaviour
 {
-   
-    // vitesse, force de saut
-        public float speed = 5f;
-        public float jumpForce = 8f;
+    // vitesse de déplacement
+    public float speed = 5f;
 
+    [Header("ANIMATION")]
+    public Sprite spriteIdle;
+    public Sprite spriteJump;
+    public float spriteChangeDelay = 0.1f;
 
-        [Header("ANIMATION")]
-        public Sprite spriteIdle;
-        public Sprite spriteJump;
-        public float spriteChangeDelay = 0.1f;
+    public float _spriteLastChange = 0f;
+    private int _spriteIndex = 0;
+    public Sprite[] spritesWalk;
 
-        public float _spriteLastChange = 0f;
-        private int _spriteIndex = 0;
-        public Sprite[] spritesWalk ;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
-
-
-        private Rigidbody2D rb;
-        private SpriteRenderer sprite;
-
-
-    // Update is called once per frame
     void Update()
     {
         _spriteLastChange += Time.deltaTime;
 
-        if (Mathf.Abs(rb.velocity.y) > 0.2f){
-             sprite.sprite = spriteJump;
-
-
-        }else if (Mathf.Abs(rb.velocity.x) > 0.2f){
-
-            if (_spriteLastChange >= spriteChangeDelay){
+        // Gestion de l'animation
+        if (Mathf.Abs(rb.velocity.magnitude) > 0.2f) // Vérifie si le personnage se déplace
+        {
+            if (_spriteLastChange >= spriteChangeDelay)
+            {
                 _spriteLastChange = 0f;
                 _spriteIndex = (_spriteIndex + 1) % spritesWalk.Length;
                 sprite.sprite = spritesWalk[_spriteIndex];
             }
-        } else {
+        }
+        else
+        {
             sprite.sprite = spriteIdle;
         }
-        
     }
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //rb.velocity = new Vector2(speed, 0);
         sprite = GetComponent<SpriteRenderer>();
-    }
 
-    private void FixedUpdate(){
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-        if(moveInput != 0)
-            sprite.flipX = moveInput < 0;
-
-        // Axe vertical > 0 jumpForce
-        float jumpInput = Input.GetAxis("Vertical");
-        if (jumpInput > 0 && Mathf.Abs(rb.velocity.y) < .001f){
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
         
-
+        rb.gravityScale = 0;
     }
 
-    
+    private void FixedUpdate()
+    {
+      
+        float moveInputX = Input.GetAxis("Horizontal");
+        float moveInputY = Input.GetAxis("Vertical");
+
+        // Appliquer la vitesse
+        rb.velocity = new Vector2(moveInputX * speed, moveInputY * speed);
+
+        // Gérer l'orientation du sprite
+        if (moveInputX != 0)
+            sprite.flipX = moveInputX < 0;
+    }
 }
