414a8ab77f3704f35aec6bc8d890e53d07aa53d6
U-Gabriel <gabrielarie2000@gmail.com>
2024-12-19 23:20:11 +0100

    Add - Scenes Loading && Menu

---
+++ Assets/LoadingScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public string nextSceneName = "Menu";
+
+    void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(3); // Simule un temps de chargement
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
